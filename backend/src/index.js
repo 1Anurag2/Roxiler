34ad9959331e28ed5054464621d6f919c5d22801
Anurag Verma <147230214+1Anurag2@users.noreply.github.com>
2026-05-23 11:44:56 +0530

    final commit

--- backend/src/index.js
+++ backend/src/index.js
@@ -19,8 +19,10 @@ app.use('/api/admin', adminRoutes);
 app.use('/api/users', userRoutes);
 app.use('/api/owner', ownerRoutes);
 
-const PORT = process.env.PORT || 5000;
 
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
+app.get("/", (req, res) => {
+  res.send("API Running");
 });
+
+
+module.exports = app;
